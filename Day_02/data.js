6d387b3ba7c9127eab503b8471d208fe7bec3e87
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-07-03 10:35:50 +0530

    rename Student

--- Day_02/data.js
+++ Day_02/data.js
@@ -1,6 +1,6 @@
 const user = [
     {id: 1, name: "Githmi Thathsarani♟️", role: "Admin"},
-    {id: 2, name: "Student", role: "User"},
+    {id: 2, name: "Student💻", role: "User"},
 ];
 
 module.exports = user;
